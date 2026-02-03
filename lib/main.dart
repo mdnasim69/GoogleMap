7185962ee516931e117219863dff2c4e05ab05b4
Nasim <mdnasimnasim2007@gmail.com>
2026-02-03 20:30:19 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  MapType _mapType = MapType.normal;
+  String name = "GoogleMap";
+  GoogleMapController? _googleMapController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+        actions: [
+          PopupMenuButton<MapType>(
+            icon: const Icon(Icons.layers),
+            onSelected: (MapType type) {
+              setState(() {
+                _mapType = type;
+              });
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem(value: MapType.normal, child: Text("Normal")),
+              const PopupMenuItem(
+                value: MapType.satellite,
+                child: Text("Satellite"),
+              ),
+              const PopupMenuItem(value: MapType.hybrid, child: Text("Hybrid")),
+              const PopupMenuItem(
+                value: MapType.terrain,
+                child: Text("Terrain"),
+              ),
+            ],
+          ),
+          PopupMenuButton<String>(
+            icon: Icon(Icons.ac_unit_rounded),
+            onSelected: (v) {
+              name = v;
+              setState(() {});
+            },
+            itemBuilder: (context) => [
+              PopupMenuItem(value: 'Microsoft', child: Text("Microsoft")),
+              PopupMenuItem(value: 'Facebook', child: Text("Facebook")),
+              PopupMenuItem(value: 'Youtube', child: Text("Youtube")),
+            ],
+          ),
+        ],
+      ),
+      body: GoogleMap(
+        mapType: _mapType,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(24.3746, 88.6004),
+          zoom: 16,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _googleMapController = controller;
+        },
+      ),
+    );
+  }
+}
