028fd667b328c73631bdb20d9932e7f0811187f6
Nasim <mdnasimnasim2007@gmail.com>
2026-02-04 20:25:44 +0600

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -32,11 +33,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   MapType _mapType = MapType.normal;
   String name = "GoogleMap";
+
   //Map controller
   GoogleMapController? _googleMapController;
+
   // Marker set
   final Set<Marker> _markers = {
     Marker(
@@ -46,10 +48,37 @@ class _HomeState extends State<Home> {
     ),
   };
 
+  TextEditingController _searchController = TextEditingController();
+
+  //searched places list
+  List<dynamic> _suggestions = [
+  ];
+
+  // Search places Api get request and Get places List
+  String apiKey = "AIzaSyBpD1p4hQ3su-aayuiQ226ZDXQRqK1cIok";
+
+  Future<List<dynamic>> placeAutocomplete(String query) async {
+    final url =
+        "https://maps.googleapis.com/maps/api/place/autocomplete/json"
+        "?input=$query"
+        "&key=$apiKey"
+        "&components=country:bd";
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)['predictions'];
+    } else {
+      return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
+        toolbarHeight: 30,
         title: Text(name),
         actions: [
           //Map Layer
@@ -76,48 +105,119 @@ class _HomeState extends State<Home> {
         ],
       ),
       //Show Map
-      body: GoogleMap(
-        myLocationEnabled: true,
-        myLocationButtonEnabled: false,
-        mapType: _mapType,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(24.3746, 88.6004),
-          zoom: 16,
-        ),
-        onMapCreated: (GoogleMapController controller) {
-          _googleMapController = controller;
-        },
-        trafficEnabled: true,
-        markers: _markers,
-        onTap: (LatLng position) async {
-          try {
-            //lat long to places
-            List<Placemark> placemark = await placemarkFromCoordinates(
-              position.latitude,
-              position.longitude,
-            );
-            Placemark place = placemark.reversed.last;
-
-            String address =
-                "${place.subLocality}, ${place.locality}, ${place.country},";
-            //place to lat long
-            List<Location> locations = await locationFromAddress(address);
-            //add marker to markers set
-            _markers.add(
-              Marker(
-                position: position,
-                infoWindow: InfoWindow(
-                  title:
-                      "${locations.first.latitude} , ${locations.first.longitude} , $address",
-                ),
-                markerId: MarkerId(position.toString()),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                hintText: "Search place",
+                prefixIcon: Icon(Icons.search),
               ),
-            );
-            setState(() {});
-          } catch (e) {
-            print(e.toString());
-          }
-        },
+              onChanged: (value) async {
+                if (value.isNotEmpty) {
+                  _suggestions = await placeAutocomplete(value);
+                  setState(() {});
+                } else {
+                  _suggestions.clear();
+                  setState(() {});
+                }
+              },
+            ),
+          ),
+          Expanded(
+            child: Stack(
+              children: [
+                GoogleMap(
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: false,
+                  mapType: _mapType,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(24.3746, 88.6004),
+                    zoom: 16,
+                  ),
+                  onMapCreated: (GoogleMapController controller) {
+                    _googleMapController = controller;
+                  },
+                  trafficEnabled: true,
+                  markers: _markers,
+                  onTap: (LatLng position) async {
+                    try {
+                      //lat long to places
+                      List<Placemark> placemark =
+                          await placemarkFromCoordinates(
+                            position.latitude,
+                            position.longitude,
+                          );
+                      Placemark place = placemark.reversed.last;
+
+                      String address =
+                          "${place.subLocality}, ${place.locality}, ${place.country},";
+                      //place to lat long
+                      List<Location> locations = await locationFromAddress(
+                        address,
+                      );
+                      //add marker to markers set
+                      _markers.add(
+                        Marker(
+                          position: position,
+                          infoWindow: InfoWindow(
+                            title:
+                                "${locations.first.latitude} , ${locations.first.longitude} , $address",
+                          ),
+                          markerId: MarkerId(position.toString()),
+                        ),
+                      );
+                      setState(() {});
+                    } catch (e) {
+                      print(e.toString());
+                    }
+                  },
+                ),
+                //Searched places List View
+                if (_suggestions.isNotEmpty)
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      color: Colors.black54,
+                      height: 200,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: _suggestions.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            color: Colors.black54,
+                            child: ListTile(
+                              title: Text(
+                                _suggestions[index]['description'],
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              onTap: () {
+                                // print(_suggestions[index]['place_id']);
+                                _suggestions.clear();
+                                setState(() {});
+                                _googleMapController?.animateCamera(
+                                  CameraUpdate.newCameraPosition(
+                                    CameraPosition(
+                                      zoom: 16,
+                                      target: LatLng(24.7413, 88.2912),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -142,14 +242,19 @@ class _HomeState extends State<Home> {
           // // navigate to place using lat long
           _googleMapController?.animateCamera(
             CameraUpdate.newCameraPosition(
-              CameraPosition(target: LatLng(24.7413, 88.2912),zoom:16),
+              CameraPosition(target: LatLng(24.7413, 88.2912), zoom: 16),
             ),
           );
         },
+        backgroundColor: Colors.yellow,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Icon(Icons.location_on_outlined),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
   }
-//Location Permission and get current location lat long
+
+  //Location Permission and get current location lat long
   Future<Position> getCurrentLocation() async {
     var location = await Geolocator.requestPermission();
     return await Geolocator.getCurrentPosition();
